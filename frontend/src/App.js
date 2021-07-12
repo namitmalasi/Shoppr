704cc8d746232cfbf9fc42ffd823789810280868
Namit Malasi <namitmalasi@gmail.com>
2021-07-12 18:55:40 +0530

    React-Bootstrap setup and Header and Footercomponents

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+
 function App() {
   return (
     <>
-      <h1>Welcome to Shoppr</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Shoppr</h1>
+        </Container>
+      </main>
+
+      <Footer />
     </>
   );
 }
