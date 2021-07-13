68b8e9f53198db68e4e87b84f91263f2e9d2366b
Namit Malasi <namitmalasi@gmail.com>
2021-07-13 12:10:10 +0530

    added product listing homescreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to Shoppr</h1>
+          <HomeScreen />
         </Container>
       </main>
 
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import Product from "../components/Product";
+import products from "../products";
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h1>Homescreen</h1>
+      <Row>
+        {products.map((product) => {
+          return (
+            <Col sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          );
+        })}
+      </Row>
+    </>
+  );
+};
+
+export default HomeScreen;
