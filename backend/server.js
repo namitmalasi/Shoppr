1ea6ed0a890d787a973969b33a60141625ee2a2d
Namit Malasi <namitmalasi@gmail.com>
2021-07-14 13:38:33 +0530

    settung up the mongodb database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
-const express = require("express");
-const products = require("./data/products");
+import express from "express";
+import products from "./data/products.js";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
 const app = express();
+
+dotenv.config();
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Api is running...");
@@ -15,6 +22,7 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, () => {
-  console.log("server running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server running in ${process.env.NODE_ENV} mode on port ${5000}`);
 });
