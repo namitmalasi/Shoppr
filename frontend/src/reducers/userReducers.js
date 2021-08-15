d8a36e679f65997acb4c31a03952797f653da573
Namit Malasi <namitmalasi@gmail.com>
2021-08-15 22:03:23 +0530

    added the admin screen access

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -16,6 +16,7 @@ import {
   USER_LIST_REQUEST,
   USER_LIST_SUCCESS,
   USER_LIST_FAIL,
+  USER_LIST_RESET,
 } from "../constants/userConstants.js";
 
 export const userLoginReducer = (state = {}, action) => {
@@ -97,6 +98,10 @@ export const userListReducer = (state = { users: [] }, action) => {
     case USER_LIST_FAIL: {
       return { loading: false, error: action.payload };
     }
+    case USER_LIST_RESET:
+      return {
+        users: [],
+      };
     default:
       return state;
   }
